1420f1a452abff9791f29ec6b152f10bc05bb10c
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2022-07-15 20:15:35 +0100

    Reverse URLs

--- django-project/my_site/first_app/views.py
+++ django-project/my_site/first_app/views.py
@@ -1,6 +1,7 @@
 # from django.http.response import HttpResponse, HttpResponseNotFound
-from django.http.response import Http404, HttpResponse
+from django.http.response import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render  # noqa: F401
+from django.urls import reverse
 
 # Create your views here.
 
@@ -20,6 +21,15 @@ def news_view(request, topic):
         # result = 'No page for that topic!'
         # return HttpResponseNotFound(result)
         raise Http404('404 GENERIC ERROR')
+
+
+def num_page_view(request, num_page):
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+
+    # webpage = reverse('topic-page', args=[topic])
+    # return HttpResponseRedirect(webpage)
+    return HttpResponseRedirect(reverse('topic-page', args=[topic]))
 
 
 def add_view(request, num1, num2):
