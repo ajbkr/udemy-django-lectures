91a5b399839150b30c5e69e7cd1e8b12f1b08218
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2022-07-15 18:34:22 +0100

    Function-based views

--- django-project/my_site/first_app/views.py
+++ django-project/my_site/first_app/views.py
@@ -1,3 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render  # noqa: F401
 
 # Create your views here.
+
+
+def simple_view(request):
+    return HttpResponse('SIMPLE VIEW')
