8eb2d018af252333a619edd8e28f7088cdf38dd6
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2022-07-29 20:05:33 +0100

    Update models

--- django-models-project/my_site/office/models.py
+++ django-models-project/my_site/office/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -7,4 +8,12 @@ from django.db import models
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
+    age = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(120)]
+    )
+    heart_rate = models.IntegerField(
+        default=60, validators=[MinValueValidator(1), MaxValueValidator(300)]
+    )
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name} is {self.age} years old.'
