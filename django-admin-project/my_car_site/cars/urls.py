99983e8ebd47772bd43a31d2751f6147047190aa
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2022-07-30 22:40:02 +0100

    Cars Web site

---
+++ django-admin-project/my_car_site/cars/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'cars'
+
+urlpatterns = [
+  path('add/', views.add, name='add'),
+  path('delete/', views.delete, name='delete'),
+  path('list/', views.list, name='list'),
+]
---
+++ django-admin-project/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python my_car_site/manage.py runserver
