e334f8b6200f10b971a04d7c0c40b8385c0058c1
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2022-08-15 21:41:19 +0100

    TemplateView

---
+++ django-cbv-project/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python school/manage.py runserver
---
+++ django-cbv-project/school/classroom/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'classroom/home.html'
+
+
+class ThankYouView(TemplateView):
+    template_name = 'classroom/thank_you.html'
