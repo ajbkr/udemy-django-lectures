f2cf2903d0887e0e21c44e0be63b2f200d8fe035
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2022-08-17 20:47:52 +0100

    FormView

--- django-cbv-project/school/classroom/urls.py
+++ django-cbv-project/school/classroom/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, ThankYouView
+from .views import ContactFormView, HomeView, ThankYouView
 
 
 app_name = 'classroom'
@@ -8,4 +8,5 @@ app_name = 'classroom'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('thank_you/', ThankYouView.as_view(), name='thank_you'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
 ]
--- django-cbv-project/school/classroom/views.py
+++ django-cbv-project/school/classroom/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
+
+from classroom.forms import ContactForm
 
 
 class HomeView(TemplateView):
@@ -7,3 +10,15 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = 'classroom/thank_you.html'
+
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+
+    success_url = reverse_lazy('classroom:thank_you')
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+
+        return super().form_valid(form)
